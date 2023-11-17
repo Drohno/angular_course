9cbf59c520c71eba8a8e0c5963ce387e59dc5a6e
David Roldan <david.roldan.nocelo@gmail.com>
2023-11-17 10:43:56 +0100

    basics assignment 1

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `<h1>Alarm!</h1>`,
+  styles: `h1 {
+    color: red;
+  }`,
+})
+export class WarningAlertComponent {}
