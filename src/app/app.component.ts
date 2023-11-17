50a338247454e1fca452133d13b8fd8fc5910e9b
David Roldan <david.roldan.nocelo@gmail.com>
2023-11-17 11:29:21 +0100

    feat: exercise 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  displayDetails = false;
+  indexToApplyStyleAndClassConditionally = 4;
+  arrayOfLogs = [];
+
+  toggleDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.arrayOfLogs.push({ timestamp: new Date().toISOString() });
+  }
 }
